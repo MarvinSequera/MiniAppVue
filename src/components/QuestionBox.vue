bb2fc5cdb1390de9b31aabd1f14208004dc99ae3
MarvinSequera <marvinsequera@yahoo.com>
2020-04-17 10:24:27 +0200

    Correcion de seguridad

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -4,7 +4,13 @@
       <template v-slot:lead>{{question.question}}</template>
       <hr class="my-4" />
       <b-list-group>
-        <b-list-group-item v-for="(answer,index) in answers" :key="index">{{answer}}</b-list-group-item>
+        <b-list-group-item
+          class="list-group"
+          v-for="(answer,index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+        >{{answer}}</b-list-group-item>
       </b-list-group>
       <b-button class="button" variant="primary">Submit</b-button>
       <b-button class="button" @click="next" variant="success">Next</b-button>
@@ -17,11 +23,21 @@ export default {
     question: Object,
     next: Function
   },
+  data() {
+    return {
+      selectedIndex: null
+    };
+  },
   computed: {
     answers() {
       let answers = [...this.question.incorrect_answers];
       answers.push(this.question.correct_answer);
       return answers;
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
     }
   }
 };
@@ -32,5 +48,18 @@ export default {
   margin: 10px 5px;
   width: 80px;
   text-align: center;
+}
+.list-group:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: red;
 }
 </style>
